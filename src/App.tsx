3506857f48708ae2eda694cd1de054cafa9d9ad7
EricKim987 <ericjin987@gmail.com>
2025-06-16 20:07:15 +0800

    Add some content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="h-16 grid justify-items-center">
+        <div className="w-full max-w-[980px] grid grid-cols-[auto_1fr_auto]">
+          <div className="p-4">CW</div>
+          <div aria-hidden="true"></div>
+          <div className="p-4 grid grid-cols-[auto_auto] gap-4">
+            <div>Something</div>
+            <div>About</div>
+          </div>
+        </div>
+      </header>
+      <main className="grid place-items-center min-h-screen">
+        <h1>Coming Soon...</h1>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
