d1aed3c04dab82afc6c4c676ab3317be25ae4b12
EricKim987 <ericjin987@gmail.com>
2025-06-19 20:32:56 +0800

    Created "fool" card

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,18 @@ function App() {
   return (
     <div className="h-screen bg-black">
       <div className="max-w-[1000px] h-screen border mx-auto relative">
-        <motion.img
-          src="the-fool-380_640.webp"
-          alt="the fool card"
-          srcSet="the-fool-380_640.webp, the-fool-1141_1920.webp 2x"
-          className="bg-white absolute left-[50px] top-[50%] translate-y-[-50%] h-[320px] w-[180px] md:h-[480px] md:w-[270px]"
-        />
+        <motion.div
+          className="bg-white absolute left-[100px] top-[50%] translate-y-[-50%] py-2 px-1 rounded"
+          initial={{ y: "-100%" }}
+          animate={{ y: "0%", rotateX: "45deg", rotateZ: "10deg" }}
+        >
+          <img
+            src="the-fool-380_640.webp"
+            alt="the fool card"
+            srcSet="the-fool-380_640.webp, the-fool-1141_1920.webp 2x"
+            className="h-[160px] w-[85px] md:h-[320px] md:w-[190px]"
+          />
+        </motion.div>
       </div>
     </div>
   );
